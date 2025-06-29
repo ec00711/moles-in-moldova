a02b20df34d7b2c42a732eab4bf25c088b994927
ec00711 <105239723+ec00711@users.noreply.github.com>
2025-06-29 14:32:02 +0100

    Populate locations from js

--- js/script.js
+++ js/script.js
@@ -1,19 +1,21 @@
 const data_array = [];
 
-function AddItem(things, place, dateStr){
-    data_array.push({things: things, place: place, date: new Date(dateStr)});
+function AddItem(things, place, dateStr, leftPC, topPC){
+    data_array.push({things: things, place: place, date: new Date(dateStr), left: leftPC, top: topPC});
 }
 
-// Add items
-AddItem("moles", "Moldova", "2025-06-06");
-AddItem("dogs", "Denmark", "2025-07-07");
+// Generate data
+AddItem("moles", "Moldova", "2025-06-06", 54, 38);
+AddItem("dogs", "Denmark", "2025-07-07", 48.75, 33);
 
-// Generate table
-var tableHTML = "";
+// Add locations
 data_array.forEach((entry) => {
-        tableHTML += "<tr>";
-        tableHTML += "<td>" + entry.things + "</td>";
-        tableHTML += "<td>" + entry.place + "</td>";
-        tableHTML += "<td>" + entry.date + "</td></tr>";
-        });
-//document.getElementById("data").innerHTML = tableHTML;
+    var loc = document.createElement("img");
+
+    loc.classList.add("location");
+    loc.src = "assets/location.svg";
+    loc.style.top = entry.top + "%";
+    loc.style.left = entry.left + "%";
+
+    document.getElementById("locations").appendChild(loc);
+});
