0a9a6a89ec5c5b76ef7d1bdde6ce9805f9eaa19a
ec00711 <105239723+ec00711@users.noreply.github.com>
2025-07-20 12:36:26 +0100

    v1.1.0: Support zooming

--- js/script.js
+++ js/script.js
@@ -11,7 +11,7 @@ function printMousePos(event) {
 }
 
 function AddItem(things, place, who, dateStr, x, y){
-    data_array.push({things: things, place: place, who: who, date: new Date(dateStr), left: x*100, top: y*100});
+    data_array.push({things: things, place: place, who: who, date: new Date(dateStr), left: x, top: y});
 }
 
 function showPopup(event, entry){
@@ -48,15 +48,15 @@ AddItem("rats", "Rotterdam", "E", "2025-06-19", 0.4729, 0.3722);
 AddItem("gibbons", "Gabon", "E", "2025-06-22", 0.495, 0.661);
 AddItem("ravens", "Romania", "F", "2025-06-22", 0.530, 0.417);
 AddItem("morris men", "Manchester", "E", "2025-06-23", 0.4547, 0.3629);
-AddItem("lemmings", "Lancre", "F", "2025-06-23", 0.5, 1.1);
+//AddItem("lemmings", "Lancre", "F", "2025-06-23", 0.5, 1.1);
 AddItem("gators", "Galway", "F", "2025-06-24", 0.4355, 0.3660);
 AddItem("guinea pigs", "Ghana", "E", "2025-06-24", 0.453, 0.620);
 AddItem("kittens", "Kenya", "E", "2025-06-25", 0.574, 0.661);
 AddItem("dragons", "Denmark", "F", "2025-06-25", 0.4921, 0.3505);
-AddItem("morris dancers", "Morpork", "F", "2025-06-26", 0.4, 1.1);
+//AddItem("morris dancers", "Morpork", "F", "2025-06-26", 0.4, 1.1);
 AddItem("donkeys", "Damascus", "E", "2025-06-26", 0.5670, 0.4854);
-AddItem("harfoots", "Hobbiton", "E", "2025-06-27", 0.6, 1.1);
-AddItem("hobbits", "Hobbiton", "F", "2025-06-27", 0.65, 1.1);
+//AddItem("harfoots", "Hobbiton", "E", "2025-06-27", 0.6, 1.1);
+//AddItem("hobbits", "Hobbiton", "F", "2025-06-27", 0.65, 1.1);
 AddItem("hamsters", "Honduras", "F", "2025-06-28", 0.1976, 0.5791);
 AddItem("sheep", "San Francisco", "E", "2025-06-28", 0.0983, 0.4390);
 AddItem("dinosaurs", "Denmark", "F", "2025-06-29", 0.4869, 0.3461);
@@ -72,8 +72,8 @@ data_array.forEach((entry) => {
 
     loc.classList.add("location");
     loc.src = "assets/location.svg";
-    loc.style.top = entry.top + "%";
-    loc.style.left = entry.left + "%";
+    loc.style.top = entry.top*100 + "%";
+    loc.style.left = entry.left*100 + "%";
 
     loc.addEventListener("click", (evt) => showPopup(evt, entry));
 
